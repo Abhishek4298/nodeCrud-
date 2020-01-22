640d0025cf4d9485f8efd273dd4dee1fa59e2442
Abhishek4298 <59391928+Abhishek4298@users.noreply.github.com>
2020-01-22 15:07:55 +0530

    edit user

--- userOperation.js
+++ userOperation.js
@@ -8,14 +8,12 @@ function addUser(data) {
 	const parse = JSON.parse(rawdata);
 	let flag = true;
 	for (let i = 0; i < parse.length; i++) {
-		if( parse[i].emailid=== body.emailid ){
+		if( parse[i].emailid === body.emailid ){
 			flag = false;
 		}
 	}
-
 	if (flag) {
 		parse.push(body);
-
 		fs.writeFileSync(db, JSON.stringify(parse), function (err) {
 			if (err) throw err;
 		});
@@ -26,10 +24,10 @@ function addUser(data) {
 }
 
 function deleteUser(data) {
-	 flag = 0;
+	letflag = 0;
 	console.log("delete")
  	const body = JSON.parse(data);
- 				let rawdata = fs.readFileSync(db);
+ 	let rawdata = fs.readFileSync(db);
 	const parse = JSON.parse(rawdata);
 
  	for (let i = 0; i < parse.length; i++) {
@@ -63,17 +61,67 @@ function getUser(data)
 			flag = true;
 		}
 	}
-	if(flag){		
+		if(flag){		
 		return data1;
- 		}
-	else
-	{
+ 		}else{
 		return "no value get";
-	}
+		}
 }
+function editUser(data)
+{
+	const body = JSON.parse(data);
+  let rawdata = fs.readFileSync(db);
+	const parse = JSON.parse(rawdata);
+	
+	for (let i = 0; i < parse.length; i++)
+	{
+		flag = true;
+		if(body.id == parse[i].id){
+			console.log(body.id);
+			if(body.name != "")
+			{
+				parse[i].name = body.name;
+				flag =true;
+			}		
+			if(body.emailid != "")
+			{
+				parse[i].emailid = body.emailid;
+				flag =true;
+			}	
+			if(body.mobileno != "")
+			{
+				parse[i].mobileno = body.mobileno;
+				flag =true;
+			}	
+			if(body.age != "")
+			{
+				parse[i].age = body.age;
+				flag =true;
+			}	
+			if(body.enrollno != "")
+			{
+				parse[i].enrollno = body.enrollno;
+				flag =true;
+			}	
+		}
+	}		
+		if(flag)
+		{
+			fs.writeFile(db, JSON.stringify(parse), function (err) {
+			if (err) throw err;
+			});
+			return 'Successfully Updated user';
+		}	
+		else
+		{
+			console.log("data entry is null");
+		}
 
+
+}
 module.exports = {
 	addUser,
 	deleteUser,
-	getUser
+	getUser,
+	editUser
 }
